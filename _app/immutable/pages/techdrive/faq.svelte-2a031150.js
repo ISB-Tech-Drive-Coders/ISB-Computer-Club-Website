34c31a640063ad408d672394f8cc1292c3b16159
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2022-06-05 19:21:50 +0000

    Deploying to gh-pages from @ ISB-Tech-Drive-Coders/ISB-Computer-Club-Website@27aa2ec3101cd6c30858c611292c7a9c23aa6fd2 🚀

---
+++ _app/immutable/pages/techdrive/faq.svelte-2a031150.js
@@ -0,0 +1,59 @@
+import{S as je,i as ze,s as Le,e as s,t as a,k as d,c as i,a as h,h as n,d as r,m as c,b as p,g as Ue,J as e,n as Ee}from"../../chunks/index-d07380d5.js";function Je(We){let t,y,H,Y,E,F,D,W,G,O,C,j,z,g,L,A,I,U,J,S,M,N,x,K,R,B,V,X,b,Z,m,ee,te,k,oe,v,ae,ne,_,re,w,se,ie,Q,he,le,l,de,P,ce,ue,f,pe,me,T,ve,we,$,fe,ye;return{c(){t=s("section"),y=s("h2"),H=a("Frequently Asked Questions"),Y=d(),E=s("h4"),F=a("Q: What do we do?"),D=d(),W=s("p"),G=a(`A: We donate tech to kids who need it. If you have any old technology
+        (phones, computers, etc.) , please donate it, even if it doesn't work or
+        if it is old. Boxes are located (we are deciding a location). Our goal
+        is to stop e-waste and provide everyone with what they need to learn.`),O=d(),C=s("h4"),j=a("Q: Why should you donate?"),z=d(),g=s("p"),L=a(`A: What many people don't realize is there are many roadblocks holding
+        computers back for around a decade, and you can still use a 15-year-old
+        machine nicely now. That makes old computers great for us. Those
+        computers can help kids in need with their learning. Your old device
+        just needs a chance. `),A=s("i"),I=s("b"),U=a("Give that chance to people in need!"),J=d(),S=s("h4"),M=a("Q: What can I bring?"),N=d(),x=s("p"),K=a("A: Whatever tech you can bring is accepted!"),R=d(),B=s("h4"),V=a("Q: Is this available in other languages?"),X=d(),b=s("p"),Z=a(`A: Yes! For Spanish or Chinese translations, please send an email to:
+        `),m=s("a"),ee=a("ISBtechdrive@hotmail.com"),te=d(),k=s("p"),oe=a(`Para obtener una versi\xF3n en espa\xF1ol de este sitio web, env\xEDe un correo
+        electr\xF3nico a:
+        `),v=s("a"),ae=a("ISBtechdrive@hotmail.com"),ne=d(),_=s("p"),re=a("\u5982\u9700\u672C\u7DB2\u7AD9\u7684\u4E2D\u6587\u7248\uFF0C\u8ACB\u767C\u9001\u96FB\u5B50\u90F5\u4EF6\u81F3: "),w=s("a"),se=a("ISBtechdrive@hotmail.com"),ie=d(),Q=s("h4"),he=a("Q: Who are you partnered with?"),le=d(),l=s("p"),de=a("A: We are partnered with "),P=s("u"),ce=a("Computers with Causes"),ue=a(`. Go check them out
+        here:
+        `),f=s("a"),pe=a("https://www.computerswithcauses.org/"),me=a(`. Your donation helps the charitable programs we support, provides much
+        needed educational and vocational assets, and substantially increases
+        resources to support our communities. Donate a Computer and write it off
+        on your taxes! Business donors can deduct the underappreciated value of
+        the computer, and individuals can deduct the current market value of a
+        computer. Tax Deductions! We may be able to give you a tax receipt. We
+        take your tech and donate it to `),T=s("u"),ve=a("Computers with Causes"),we=a(`. They, in
+        turn, will give the computer to people who are in need, such as
+        low-income students who need electronic devices for online school and
+        other purposes. If your tech is broken, as much as possible will be
+        salvaged, and then the rest will be recycled into new tech.
+        iCloud\u2122 locked phones are 100% accepted! iCloud\u2122 locked
+        phones are phones which you don't have a password for and don't have any
+        data you want on it, or phones that ask for a iCloud\u2122 password.
+        Any password locked phones that don't let you use a new user profile. We
+        will send a activation request to Apple`),$=s("small"),fe=a("\xAE"),ye=a(`, with the
+        information of the phone we got. We may send you a email to remove it
+        from your iCloud\u2122 account`),this.h()},l(q){t=i(q,"SECTION",{id:!0});var o=h(t);y=i(o,"H2",{});var Ce=h(y);H=n(Ce,"Frequently Asked Questions"),Ce.forEach(r),Y=c(o),E=i(o,"H4",{});var Ae=h(E);F=n(Ae,"Q: What do we do?"),Ae.forEach(r),D=c(o),W=i(o,"P",{});var Ie=h(W);G=n(Ie,`A: We donate tech to kids who need it. If you have any old technology
+        (phones, computers, etc.) , please donate it, even if it doesn't work or
+        if it is old. Boxes are located (we are deciding a location). Our goal
+        is to stop e-waste and provide everyone with what they need to learn.`),Ie.forEach(r),O=c(o),C=i(o,"H4",{});var Se=h(C);j=n(Se,"Q: Why should you donate?"),Se.forEach(r),z=c(o),g=i(o,"P",{});var ge=h(g);L=n(ge,`A: What many people don't realize is there are many roadblocks holding
+        computers back for around a decade, and you can still use a 15-year-old
+        machine nicely now. That makes old computers great for us. Those
+        computers can help kids in need with their learning. Your old device
+        just needs a chance. `),A=i(ge,"I",{});var xe=h(A);I=i(xe,"B",{});var Be=h(I);U=n(Be,"Give that chance to people in need!"),Be.forEach(r),xe.forEach(r),ge.forEach(r),J=c(o),S=i(o,"H4",{});var Qe=h(S);M=n(Qe,"Q: What can I bring?"),Qe.forEach(r),N=c(o),x=i(o,"P",{});var Pe=h(x);K=n(Pe,"A: Whatever tech you can bring is accepted!"),Pe.forEach(r),R=c(o),B=i(o,"H4",{});var Te=h(B);V=n(Te,"Q: Is this available in other languages?"),Te.forEach(r),X=c(o),b=i(o,"P",{});var be=h(b);Z=n(be,`A: Yes! For Spanish or Chinese translations, please send an email to:
+        `),m=i(be,"A",{href:!0,target:!0});var $e=h(m);ee=n($e,"ISBtechdrive@hotmail.com"),$e.forEach(r),be.forEach(r),te=c(o),k=i(o,"P",{});var ke=h(k);oe=n(ke,`Para obtener una versi\xF3n en espa\xF1ol de este sitio web, env\xEDe un correo
+        electr\xF3nico a:
+        `),v=i(ke,"A",{href:!0,target:!0});var qe=h(v);ae=n(qe,"ISBtechdrive@hotmail.com"),qe.forEach(r),ke.forEach(r),ne=c(o),_=i(o,"P",{});var _e=h(_);re=n(_e,"\u5982\u9700\u672C\u7DB2\u7AD9\u7684\u4E2D\u6587\u7248\uFF0C\u8ACB\u767C\u9001\u96FB\u5B50\u90F5\u4EF6\u81F3: "),w=i(_e,"A",{href:!0,target:!0});var He=h(w);se=n(He,"ISBtechdrive@hotmail.com"),He.forEach(r),_e.forEach(r),ie=c(o),Q=i(o,"H4",{});var Ye=h(Q);he=n(Ye,"Q: Who are you partnered with?"),Ye.forEach(r),le=c(o),l=i(o,"P",{});var u=h(l);de=n(u,"A: We are partnered with "),P=i(u,"U",{});var Fe=h(P);ce=n(Fe,"Computers with Causes"),Fe.forEach(r),ue=n(u,`. Go check them out
+        here:
+        `),f=i(u,"A",{href:!0,target:!0});var De=h(f);pe=n(De,"https://www.computerswithcauses.org/"),De.forEach(r),me=n(u,`. Your donation helps the charitable programs we support, provides much
+        needed educational and vocational assets, and substantially increases
+        resources to support our communities. Donate a Computer and write it off
+        on your taxes! Business donors can deduct the underappreciated value of
+        the computer, and individuals can deduct the current market value of a
+        computer. Tax Deductions! We may be able to give you a tax receipt. We
+        take your tech and donate it to `),T=i(u,"U",{});var Ge=h(T);ve=n(Ge,"Computers with Causes"),Ge.forEach(r),we=n(u,`. They, in
+        turn, will give the computer to people who are in need, such as
+        low-income students who need electronic devices for online school and
+        other purposes. If your tech is broken, as much as possible will be
+        salvaged, and then the rest will be recycled into new tech.
+        iCloud\u2122 locked phones are 100% accepted! iCloud\u2122 locked
+        phones are phones which you don't have a password for and don't have any
+        data you want on it, or phones that ask for a iCloud\u2122 password.
+        Any password locked phones that don't let you use a new user profile. We
+        will send a activation request to Apple`),$=i(u,"SMALL",{});var Oe=h($);fe=n(Oe,"\xAE"),Oe.forEach(r),ye=n(u,`, with the
+        information of the phone we got. We may send you a email to remove it
+        from your iCloud\u2122 account`),u.forEach(r),o.forEach(r),this.h()},h(){p(m,"href","mailto:ISBtechdrive@hotmail.com"),p(m,"target","_blank"),p(v,"href","mailto:ISBtechdrive@hotmail.com"),p(v,"target","_blank"),p(w,"href","mailto:ISBtechdrive@hotmail.com"),p(w,"target","_blank"),p(f,"href","https://www.computerswithcauses.org/"),p(f,"target","_blank"),p(t,"id","faq")},m(q,o){Ue(q,t,o),e(t,y),e(y,H),e(t,Y),e(t,E),e(E,F),e(t,D),e(t,W),e(W,G),e(t,O),e(t,C),e(C,j),e(t,z),e(t,g),e(g,L),e(g,A),e(A,I),e(I,U),e(t,J),e(t,S),e(S,M),e(t,N),e(t,x),e(x,K),e(t,R),e(t,B),e(B,V),e(t,X),e(t,b),e(b,Z),e(b,m),e(m,ee),e(t,te),e(t,k),e(k,oe),e(k,v),e(v,ae),e(t,ne),e(t,_),e(_,re),e(_,w),e(w,se),e(t,ie),e(t,Q),e(Q,he),e(t,le),e(t,l),e(l,de),e(l,P),e(P,ce),e(l,ue),e(l,f),e(f,pe),e(l,me),e(l,T),e(T,ve),e(l,we),e(l,$),e($,fe),e(l,ye)},p:Ee,i:Ee,o:Ee,d(q){q&&r(t)}}}function Me(We,t,y){return[!0]}class Ke extends je{constructor(t){super(),ze(this,t,Me,Je,Le,{prerender:0})}get prerender(){return this.$$.ctx[0]}}export{Ke as default};
---
+++ _app/immutable/pages/techdrive/thanks.svelte-dbfb915a.js
@@ -0,0 +1,37 @@
+import{S as Ae,i as Ve,s as xe,e as t,t as p,k as h,x as pe,c as o,a as n,h as f,d as a,m as c,y as fe,b as r,g as Ce,J as e,z as me,n as qe,r as ge,p as ve,C as be}from"../../chunks/index-d07380d5.js";import{b as _e}from"../../chunks/paths-396f020f.js";import{I as Ie}from"../../chunks/ImageCarousel-4315819c.js";import"../../chunks/IconBase-5c356378.js";function Oe(M){let s,k,A,V,I,L,F,g,w,K,d,S,Q,U,E,x,C,X,Y,B,Z,ee,v,u,z,te,oe,G,q,O,ae,ne,T,se,re,y,ie,b,$,le,H,m,he,ce,de,_,ue,W;return w=new Ie({props:{images:M[0]}}),y=new Ie({props:{images:M[1]}}),$=new Ie({props:{images:M[2]}}),{c(){s=t("section"),k=t("h2"),A=p("Special Thanks"),V=h(),I=t("p"),L=p(`These are some of the people in the world that gave us this chance to
+        give others a chance.`),F=h(),g=t("div"),pe(w.$$.fragment),K=h(),d=t("div"),S=t("p"),Q=p("Andrew Gilford"),U=h(),E=t("p"),x=t("i"),C=t("b"),X=p("Principal - International School of Beaverton"),Y=h(),B=t("p"),Z=p(`If you asked me who I thought was the most helpful in putting
+                this together, the answer would be Mr. Gilford. He is kind, open
+                minded, and most important for our project, he is open to new
+                ideas! I doubt any of my other principles would ever agree to
+                this. He did. He is very interactive with the students, he does
+                a lot for us and our school. I see him daily, unlike all of my
+                previous principals. He is quite, by far, the best principle
+                I've met! He is one of the people who make ISB into the
+                incredibile school it is, big thanks to him!`),ee=h(),v=t("div"),u=t("div"),z=t("p"),te=p("Ian Bricken"),oe=h(),G=t("p"),q=t("i"),O=t("b"),ae=p(`Middle School Math Teacher - International School of
+                        Beaverton`),ne=h(),T=t("p"),se=p(`Thank you to Mr. Bricken for letting us use his room for our
+                tech area, and for encouraging our drive and letting us bring a
+                bunch of tech in his room.`),re=h(),pe(y.$$.fragment),ie=h(),b=t("div"),pe($.$$.fragment),le=h(),H=t("div"),m=t("p"),he=p(`Of course, what's the point of all of this if we didn't get
+                donations! So big round of applause for our ISB Dragons! We at
+                ISB embrace peace, and for those of you with a Samsung, or any
+                Android phone for that matter, go and use Samsung Global Goals,
+                to earn money to donate to charity by doing simple things, like
+                using a global goals background. Then choose where to donate
+                your earned money, which goal?! Learn more here:`),ce=t("br"),de=h(),_=t("a"),ue=p("Samsung Global Goals"),this.h()},l(i){s=o(i,"SECTION",{});var l=n(s);k=o(l,"H2",{});var we=n(k);A=f(we,"Special Thanks"),we.forEach(a),V=c(l),I=o(l,"P",{});var ye=n(I);L=f(ye,`These are some of the people in the world that gave us this chance to
+        give others a chance.`),ye.forEach(a),F=c(l),g=o(l,"DIV",{class:!0});var J=n(g);fe(w.$$.fragment,J),K=c(J),d=o(J,"DIV",{class:!0});var P=n(d);S=o(P,"P",{class:!0});var $e=n(S);Q=f($e,"Andrew Gilford"),$e.forEach(a),U=c(P),E=o(P,"P",{class:!0});var ke=n(E);x=o(ke,"I",{});var Se=n(x);C=o(Se,"B",{});var Ee=n(C);X=f(Ee,"Principal - International School of Beaverton"),Ee.forEach(a),Se.forEach(a),ke.forEach(a),Y=c(P),B=o(P,"P",{class:!0});var Be=n(B);Z=f(Be,`If you asked me who I thought was the most helpful in putting
+                this together, the answer would be Mr. Gilford. He is kind, open
+                minded, and most important for our project, he is open to new
+                ideas! I doubt any of my other principles would ever agree to
+                this. He did. He is very interactive with the students, he does
+                a lot for us and our school. I see him daily, unlike all of my
+                previous principals. He is quite, by far, the best principle
+                I've met! He is one of the people who make ISB into the
+                incredibile school it is, big thanks to him!`),Be.forEach(a),P.forEach(a),J.forEach(a),ee=c(l),v=o(l,"DIV",{class:!0});var N=n(v);u=o(N,"DIV",{class:!0});var D=n(u);z=o(D,"P",{class:!0});var ze=n(z);te=f(ze,"Ian Bricken"),ze.forEach(a),oe=c(D),G=o(D,"P",{class:!0});var Ge=n(G);q=o(Ge,"I",{});var Te=n(q);O=o(Te,"B",{});var He=n(O);ae=f(He,`Middle School Math Teacher - International School of
+                        Beaverton`),He.forEach(a),Te.forEach(a),Ge.forEach(a),ne=c(D),T=o(D,"P",{class:!0});var Pe=n(T);se=f(Pe,`Thank you to Mr. Bricken for letting us use his room for our
+                tech area, and for encouraging our drive and letting us bring a
+                bunch of tech in his room.`),Pe.forEach(a),D.forEach(a),re=c(N),fe(y.$$.fragment,N),N.forEach(a),ie=c(l),b=o(l,"DIV",{class:!0});var R=n(b);fe($.$$.fragment,R),le=c(R),H=o(R,"DIV",{class:!0});var De=n(H);m=o(De,"P",{class:!0});var j=n(m);he=f(j,`Of course, what's the point of all of this if we didn't get
+                donations! So big round of applause for our ISB Dragons! We at
+                ISB embrace peace, and for those of you with a Samsung, or any
+                Android phone for that matter, go and use Samsung Global Goals,
+                to earn money to donate to charity by doing simple things, like
+                using a global goals background. Then choose where to donate
+                your earned money, which goal?! Learn more here:`),ce=o(j,"BR",{}),de=c(j),_=o(j,"A",{href:!0,target:!0,class:!0});var Me=n(_);ue=f(Me,"Samsung Global Goals"),Me.forEach(a),j.forEach(a),De.forEach(a),R.forEach(a),l.forEach(a),this.h()},h(){r(S,"class","name svelte-uzn1e8"),r(E,"class","role svelte-uzn1e8"),r(B,"class","description svelte-uzn1e8"),r(d,"class","info svelte-uzn1e8"),r(g,"class","person svelte-uzn1e8"),r(z,"class","name svelte-uzn1e8"),r(G,"class","role svelte-uzn1e8"),r(T,"class","description svelte-uzn1e8"),r(u,"class","info alt svelte-uzn1e8"),r(v,"class","person svelte-uzn1e8"),r(_,"href","https://www.samsung.com/global/galaxy/apps/samsung-global-goals/"),r(_,"target","_blank"),r(_,"class","svelte-uzn1e8"),r(m,"class","description svelte-uzn1e8"),r(H,"class","info svelte-uzn1e8"),r(b,"class","person svelte-uzn1e8")},m(i,l){Ce(i,s,l),e(s,k),e(k,A),e(s,V),e(s,I),e(I,L),e(s,F),e(s,g),me(w,g,null),e(g,K),e(g,d),e(d,S),e(S,Q),e(d,U),e(d,E),e(E,x),e(x,C),e(C,X),e(d,Y),e(d,B),e(B,Z),e(s,ee),e(s,v),e(v,u),e(u,z),e(z,te),e(u,oe),e(u,G),e(G,q),e(q,O),e(O,ae),e(u,ne),e(u,T),e(T,se),e(v,re),me(y,v,null),e(s,ie),e(s,b),me($,b,null),e(b,le),e(b,H),e(H,m),e(m,he),e(m,ce),e(m,de),e(m,_),e(_,ue),W=!0},p:qe,i(i){W||(ge(w.$$.fragment,i),ge(y.$$.fragment,i),ge($.$$.fragment,i),W=!0)},o(i){ve(w.$$.fragment,i),ve(y.$$.fragment,i),ve($.$$.fragment,i),W=!1},d(i){i&&a(s),be(w),be(y),be($)}}}function je(M,s,k){const A=[{src:`${_e}/images/thanks/Gilford_1.png`,alt:"Andrew Gilford (01)",format:"png"}],V=[{src:`${_e}/images/thanks/Bricken_1.png`,alt:"Ian Bricken (01)",format:"png"}],I=[{src:`${_e}/images/thanks/ISB_1.png`,alt:"ISB (01)",format:"png"}];return[A,V,I,!0]}class Re extends Ae{constructor(s){super(),Ve(this,s,je,Oe,xe,{prerender:3})}get prerender(){return this.$$.ctx[3]}}export{Re as default};
