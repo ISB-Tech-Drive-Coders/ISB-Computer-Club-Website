7f85543ac71d4704e348e9350d5709ca93cb751b
RedstoneWizard08 <94275204+RedstoneWizard08@users.noreply.github.com>
2022-06-05 18:54:49 +0000

    Deploying to gh-pages from @ ISB-Tech-Drive-Coders/ISB-Computer-Club-Website@a854eaa42c2e159b58e8bed679a4640c301663c8 🚀

---
+++ _app/immutable/pages/techdrive/index.svelte-1111a8e8.js
@@ -0,0 +1,17 @@
+import{S as O,i as j,s as z,e as s,t as u,k as T,c as i,a as r,h as m,d as n,m as B,b as C,g as G,J as t,n as k}from"../../chunks/index-d07380d5.js";function P(W){let e,c,f,v,h,y,b,o,g,d,l,w;return{c(){e=s("section"),c=s("h2"),f=u("What is the ISB tech drive?"),v=T(),h=s("p"),y=u(`The ISB tech drive is a chance for people in need to get computers,
+        phones, tablets, and other devices. If you have any old technology
+        (phones, computers, etc.), please donate it, even if it doesn't work or
+        if it is old, because we can fix it. Our goal is to stop e-waste and
+        provide everyone with what they need to learn.`),b=T(),o=s("p"),g=u(`What many people don't realize is there are many roadblocks holding
+        computers back for around a decade, and you can still use a 15-year-old
+        machine nicely now. That makes old computers great for us, because they
+        can still help kids in need with their learning. Your old device just
+        needs a chance. `),d=s("i"),l=s("b"),w=u("Give that chance to people in need!"),this.h()},l(p){e=i(p,"SECTION",{id:!0});var a=r(e);c=i(a,"H2",{});var I=r(c);f=m(I,"What is the ISB tech drive?"),I.forEach(n),v=B(a),h=i(a,"P",{});var x=r(h);y=m(x,`The ISB tech drive is a chance for people in need to get computers,
+        phones, tablets, and other devices. If you have any old technology
+        (phones, computers, etc.), please donate it, even if it doesn't work or
+        if it is old, because we can fix it. Our goal is to stop e-waste and
+        provide everyone with what they need to learn.`),x.forEach(n),b=B(a),o=i(a,"P",{});var _=r(o);g=m(_,`What many people don't realize is there are many roadblocks holding
+        computers back for around a decade, and you can still use a 15-year-old
+        machine nicely now. That makes old computers great for us, because they
+        can still help kids in need with their learning. Your old device just
+        needs a chance. `),d=i(_,"I",{});var E=r(d);l=i(E,"B",{});var S=r(l);w=m(S,"Give that chance to people in need!"),S.forEach(n),E.forEach(n),_.forEach(n),a.forEach(n),this.h()},h(){C(e,"id","about")},m(p,a){G(p,e,a),t(e,c),t(c,f),t(e,v),t(e,h),t(h,y),t(e,b),t(e,o),t(o,g),t(o,d),t(d,l),t(l,w)},p:k,i:k,o:k,d(p){p&&n(e)}}}class q extends O{constructor(e){super(),j(this,e,null,P,z,{})}}export{q as default};
---
+++ _app/immutable/pages/techdrive/thanks.svelte-04ffedb7.js
@@ -0,0 +1,37 @@
+import{S as Ae,i as Ve,s as xe,e as t,t as p,k as h,x as pe,c as o,a as n,h as f,d as a,m as c,y as fe,b as r,g as Ce,J as e,z as me,n as qe,r as ge,p as ve,C as be}from"../../chunks/index-d07380d5.js";import{b as _e}from"../../chunks/paths-396f020f.js";import{I as Ie}from"../../chunks/ImageCarousel-4315819c.js";import"../../chunks/IconBase-5c356378.js";function Oe(D){let s,I,M,N,A,R,F,g,w,K,d,$,Q,U,S,V,x,X,Y,E,Z,ee,v,u,B,te,oe,z,C,q,ae,ne,G,se,re,y,ie,b,k,le,T,m,he,ce,de,_,ue,j;return w=new Ie({props:{images:D[0]}}),y=new Ie({props:{images:D[1]}}),k=new Ie({props:{images:D[2]}}),{c(){s=t("section"),I=t("h2"),M=p("Special Thanks"),N=h(),A=t("p"),R=p(`These are some of the people in the world that gave us this chance to
+        give others a chance.`),F=h(),g=t("div"),pe(w.$$.fragment),K=h(),d=t("div"),$=t("p"),Q=p("Andrew Gilford"),U=h(),S=t("p"),V=t("i"),x=t("b"),X=p("Principal - International School of Beaverton"),Y=h(),E=t("p"),Z=p(`If you asked me who I thought was the most helpful in putting
+                this together, the answer would be Mr. Gilford. He is kind, open
+                minded, and most important for our project, he is open to new
+                ideas! I doubt any of my other principles would ever agree to
+                this. He did. He is very interactive with the students, he does
+                a lot for us and our school. I see him daily, unlike all of my
+                previous principals. He is quite, by far, the best principle
+                I've met! He is one of the people who make ISB into the
+                incredibile school it is, big thanks to him!`),ee=h(),v=t("div"),u=t("div"),B=t("p"),te=p("Ian Bricken"),oe=h(),z=t("p"),C=t("i"),q=t("b"),ae=p(`Middle School Math Teacher - International School of
+                        Beaverton`),ne=h(),G=t("p"),se=p(`Thank you to Mr. Bricken for letting us use his room for our
+                tech area, and for encouraging our drive and letting us bring a
+                bunch of tech in his room.`),re=h(),pe(y.$$.fragment),ie=h(),b=t("div"),pe(k.$$.fragment),le=h(),T=t("div"),m=t("p"),he=p(`Of course, what's the point of all of this if we didn't get
+                donations! So big round of applause for our ISB Dragons! We at
+                ISB embrace peace, and for those of you with a Samsung, or any
+                Android phone for that matter, go and use Samsung Global Goals,
+                to earn money to donate to charity by doing simple things, like
+                using a global goals background. Then choose where to donate
+                your earned money, which goal?! Learn more here:`),ce=t("br"),de=h(),_=t("a"),ue=p("Samsung Global Goals"),this.h()},l(i){s=o(i,"SECTION",{});var l=n(s);I=o(l,"H2",{});var we=n(I);M=f(we,"Special Thanks"),we.forEach(a),N=c(l),A=o(l,"P",{});var ye=n(A);R=f(ye,`These are some of the people in the world that gave us this chance to
+        give others a chance.`),ye.forEach(a),F=c(l),g=o(l,"DIV",{class:!0});var L=n(g);fe(w.$$.fragment,L),K=c(L),d=o(L,"DIV",{class:!0});var H=n(d);$=o(H,"P",{class:!0});var ke=n($);Q=f(ke,"Andrew Gilford"),ke.forEach(a),U=c(H),S=o(H,"P",{class:!0});var $e=n(S);V=o($e,"I",{});var Se=n(V);x=o(Se,"B",{});var Ee=n(x);X=f(Ee,"Principal - International School of Beaverton"),Ee.forEach(a),Se.forEach(a),$e.forEach(a),Y=c(H),E=o(H,"P",{class:!0});var Be=n(E);Z=f(Be,`If you asked me who I thought was the most helpful in putting
+                this together, the answer would be Mr. Gilford. He is kind, open
+                minded, and most important for our project, he is open to new
+                ideas! I doubt any of my other principles would ever agree to
+                this. He did. He is very interactive with the students, he does
+                a lot for us and our school. I see him daily, unlike all of my
+                previous principals. He is quite, by far, the best principle
+                I've met! He is one of the people who make ISB into the
+                incredibile school it is, big thanks to him!`),Be.forEach(a),H.forEach(a),L.forEach(a),ee=c(l),v=o(l,"DIV",{class:!0});var W=n(v);u=o(W,"DIV",{class:!0});var P=n(u);B=o(P,"P",{class:!0});var ze=n(B);te=f(ze,"Ian Bricken"),ze.forEach(a),oe=c(P),z=o(P,"P",{class:!0});var Ge=n(z);C=o(Ge,"I",{});var Te=n(C);q=o(Te,"B",{});var He=n(q);ae=f(He,`Middle School Math Teacher - International School of
+                        Beaverton`),He.forEach(a),Te.forEach(a),Ge.forEach(a),ne=c(P),G=o(P,"P",{class:!0});var Pe=n(G);se=f(Pe,`Thank you to Mr. Bricken for letting us use his room for our
+                tech area, and for encouraging our drive and letting us bring a
+                bunch of tech in his room.`),Pe.forEach(a),P.forEach(a),re=c(W),fe(y.$$.fragment,W),W.forEach(a),ie=c(l),b=o(l,"DIV",{class:!0});var J=n(b);fe(k.$$.fragment,J),le=c(J),T=o(J,"DIV",{class:!0});var De=n(T);m=o(De,"P",{class:!0});var O=n(m);he=f(O,`Of course, what's the point of all of this if we didn't get
+                donations! So big round of applause for our ISB Dragons! We at
+                ISB embrace peace, and for those of you with a Samsung, or any
+                Android phone for that matter, go and use Samsung Global Goals,
+                to earn money to donate to charity by doing simple things, like
+                using a global goals background. Then choose where to donate
+                your earned money, which goal?! Learn more here:`),ce=o(O,"BR",{}),de=c(O),_=o(O,"A",{href:!0,target:!0,class:!0});var Me=n(_);ue=f(Me,"Samsung Global Goals"),Me.forEach(a),O.forEach(a),De.forEach(a),J.forEach(a),l.forEach(a),this.h()},h(){r($,"class","name svelte-uzn1e8"),r(S,"class","role svelte-uzn1e8"),r(E,"class","description svelte-uzn1e8"),r(d,"class","info svelte-uzn1e8"),r(g,"class","person svelte-uzn1e8"),r(B,"class","name svelte-uzn1e8"),r(z,"class","role svelte-uzn1e8"),r(G,"class","description svelte-uzn1e8"),r(u,"class","info alt svelte-uzn1e8"),r(v,"class","person svelte-uzn1e8"),r(_,"href","https://www.samsung.com/global/galaxy/apps/samsung-global-goals/"),r(_,"target","_blank"),r(_,"class","svelte-uzn1e8"),r(m,"class","description svelte-uzn1e8"),r(T,"class","info svelte-uzn1e8"),r(b,"class","person svelte-uzn1e8")},m(i,l){Ce(i,s,l),e(s,I),e(I,M),e(s,N),e(s,A),e(A,R),e(s,F),e(s,g),me(w,g,null),e(g,K),e(g,d),e(d,$),e($,Q),e(d,U),e(d,S),e(S,V),e(V,x),e(x,X),e(d,Y),e(d,E),e(E,Z),e(s,ee),e(s,v),e(v,u),e(u,B),e(B,te),e(u,oe),e(u,z),e(z,C),e(C,q),e(q,ae),e(u,ne),e(u,G),e(G,se),e(v,re),me(y,v,null),e(s,ie),e(s,b),me(k,b,null),e(b,le),e(b,T),e(T,m),e(m,he),e(m,ce),e(m,de),e(m,_),e(_,ue),j=!0},p:qe,i(i){j||(ge(w.$$.fragment,i),ge(y.$$.fragment,i),ge(k.$$.fragment,i),j=!0)},o(i){ve(w.$$.fragment,i),ve(y.$$.fragment,i),ve(k.$$.fragment,i),j=!1},d(i){i&&a(s),be(w),be(y),be(k)}}}function je(D){const s=[{src:`${_e}/images/thanks/Gilford_1.png`,alt:"Andrew Gilford (01)",format:"png"}],I=[{src:`${_e}/images/thanks/Bricken_1.png`,alt:"Ian Bricken (01)",format:"png"}],M=[{src:`${_e}/images/thanks/ISB_1.png`,alt:"ISB (01)",format:"png"}];return[s,I,M]}class Re extends Ae{constructor(s){super(),Ve(this,s,je,Oe,xe,{})}}export{Re as default};
