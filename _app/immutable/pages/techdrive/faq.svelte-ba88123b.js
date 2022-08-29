1ad5169d359e6f0a371b991c740d075e1c870ae4
ravinder-Olivier <ravinder-Olivier@outlook.com>
2022-08-29 17:37:58 +0000

    Deploying to gh-pages from @ ISB-Tech-Drive-Coders/ISB-Computer-Club-Website@6d7ce26dcc12c74c28d03f7489eb244c9d661a06 🚀

---
+++ _app/immutable/pages/techdrive/faq.svelte-ba88123b.js
@@ -0,0 +1,59 @@
+import{S as je,i as ze,s as Le,l as s,u as a,a as d,m as i,p as l,v as n,h as r,c,q as p,b as Ue,J as e,n as Ee}from"../../chunks/index-5a47a77b.js";function Je(We){let t,y,H,Y,E,F,D,W,G,O,C,j,z,g,L,A,I,U,J,S,M,N,x,K,R,B,V,X,b,Z,m,ee,te,_,oe,v,ae,ne,k,re,w,se,ie,Q,le,he,h,de,P,ce,ue,f,pe,me,T,ve,we,q,fe,ye;return{c(){t=s("section"),y=s("h2"),H=a("Frequently Asked Questions"),Y=d(),E=s("h4"),F=a("Q: What do we do?"),D=d(),W=s("p"),G=a(`A: We donate tech to kids who need it. If you have any old technology
+        (phones, computers, etc.) , please donate it, even if it doesn't work or
+        if it is old. Boxes are located (we are deciding a location). Our goal
+        is to stop e-waste and provide everyone with what they need to learn.`),O=d(),C=s("h4"),j=a("Q: Why should you donate?"),z=d(),g=s("p"),L=a(`A: What many people don't realize is there are many roadblocks holding
+        computers back for around a decade, and you can still use a 15-year-old
+        machine nicely now. That makes old computers great for us. Those
+        computers can help kids in need with their learning. Your old device
+        just needs a chance. `),A=s("i"),I=s("b"),U=a("Give that chance to people in need!"),J=d(),S=s("h4"),M=a("Q: What can I bring?"),N=d(),x=s("p"),K=a("A: Whatever tech you can bring is accepted!"),R=d(),B=s("h4"),V=a("Q: Is this available in other languages?"),X=d(),b=s("p"),Z=a(`A: Yes! For Spanish or Chinese translations, please send an email to:
+        `),m=s("a"),ee=a("ISBtechdrive@hotmail.com"),te=d(),_=s("p"),oe=a(`Para obtener una versi\xF3n en espa\xF1ol de este sitio web, env\xEDe un correo
+        electr\xF3nico a:
+        `),v=s("a"),ae=a("ISBtechdrive@hotmail.com"),ne=d(),k=s("p"),re=a("\u5982\u9700\u672C\u7DB2\u7AD9\u7684\u4E2D\u6587\u7248\uFF0C\u8ACB\u767C\u9001\u96FB\u5B50\u90F5\u4EF6\u81F3: "),w=s("a"),se=a("ISBtechdrive@hotmail.com"),ie=d(),Q=s("h4"),le=a("Q: Who are you partnered with?"),he=d(),h=s("p"),de=a("A: We are partnered with "),P=s("u"),ce=a("Computers with Causes"),ue=a(`. Go check them out
+        here:
+        `),f=s("a"),pe=a("https://www.computerswithcauses.org/"),me=a(`. Your donation helps the charitable programs we support, provides much
+        needed educational and vocational assets, and substantially increases
+        resources to support our communities. Donate a Computer and write it off
+        on your taxes! Business donors can deduct the underappreciated value of
+        the computer, and individuals can deduct the current market value of a
+        computer. Tax Deductions! We may be able to give you a tax receipt. We
+        take your tech and donate it to `),T=s("u"),ve=a("Computers with Causes"),we=a(`. They, in
+        turn, will give the computer to people who are in need, such as
+        low-income students who need electronic devices for online school and
+        other purposes. If your tech is broken, as much as possible will be
+        salvaged, and then the rest will be recycled into new tech.
+        iCloud\u2122 locked phones are 100% accepted! iCloud\u2122 locked
+        phones are phones which you don't have a password for and don't have any
+        data you want on it, or phones that ask for a iCloud\u2122 password.
+        Any password locked phones that don't let you use a new user profile. We
+        will send a activation request to Apple`),q=s("small"),fe=a("\xAE"),ye=a(`, with the
+        information of the phone we got. We may send you a email to remove it
+        from your iCloud\u2122 account`),this.h()},l($){t=i($,"SECTION",{id:!0});var o=l(t);y=i(o,"H2",{});var Ce=l(y);H=n(Ce,"Frequently Asked Questions"),Ce.forEach(r),Y=c(o),E=i(o,"H4",{});var Ae=l(E);F=n(Ae,"Q: What do we do?"),Ae.forEach(r),D=c(o),W=i(o,"P",{});var Ie=l(W);G=n(Ie,`A: We donate tech to kids who need it. If you have any old technology
+        (phones, computers, etc.) , please donate it, even if it doesn't work or
+        if it is old. Boxes are located (we are deciding a location). Our goal
+        is to stop e-waste and provide everyone with what they need to learn.`),Ie.forEach(r),O=c(o),C=i(o,"H4",{});var Se=l(C);j=n(Se,"Q: Why should you donate?"),Se.forEach(r),z=c(o),g=i(o,"P",{});var ge=l(g);L=n(ge,`A: What many people don't realize is there are many roadblocks holding
+        computers back for around a decade, and you can still use a 15-year-old
+        machine nicely now. That makes old computers great for us. Those
+        computers can help kids in need with their learning. Your old device
+        just needs a chance. `),A=i(ge,"I",{});var xe=l(A);I=i(xe,"B",{});var Be=l(I);U=n(Be,"Give that chance to people in need!"),Be.forEach(r),xe.forEach(r),ge.forEach(r),J=c(o),S=i(o,"H4",{});var Qe=l(S);M=n(Qe,"Q: What can I bring?"),Qe.forEach(r),N=c(o),x=i(o,"P",{});var Pe=l(x);K=n(Pe,"A: Whatever tech you can bring is accepted!"),Pe.forEach(r),R=c(o),B=i(o,"H4",{});var Te=l(B);V=n(Te,"Q: Is this available in other languages?"),Te.forEach(r),X=c(o),b=i(o,"P",{});var be=l(b);Z=n(be,`A: Yes! For Spanish or Chinese translations, please send an email to:
+        `),m=i(be,"A",{href:!0,target:!0});var qe=l(m);ee=n(qe,"ISBtechdrive@hotmail.com"),qe.forEach(r),be.forEach(r),te=c(o),_=i(o,"P",{});var _e=l(_);oe=n(_e,`Para obtener una versi\xF3n en espa\xF1ol de este sitio web, env\xEDe un correo
+        electr\xF3nico a:
+        `),v=i(_e,"A",{href:!0,target:!0});var $e=l(v);ae=n($e,"ISBtechdrive@hotmail.com"),$e.forEach(r),_e.forEach(r),ne=c(o),k=i(o,"P",{});var ke=l(k);re=n(ke,"\u5982\u9700\u672C\u7DB2\u7AD9\u7684\u4E2D\u6587\u7248\uFF0C\u8ACB\u767C\u9001\u96FB\u5B50\u90F5\u4EF6\u81F3: "),w=i(ke,"A",{href:!0,target:!0});var He=l(w);se=n(He,"ISBtechdrive@hotmail.com"),He.forEach(r),ke.forEach(r),ie=c(o),Q=i(o,"H4",{});var Ye=l(Q);le=n(Ye,"Q: Who are you partnered with?"),Ye.forEach(r),he=c(o),h=i(o,"P",{});var u=l(h);de=n(u,"A: We are partnered with "),P=i(u,"U",{});var Fe=l(P);ce=n(Fe,"Computers with Causes"),Fe.forEach(r),ue=n(u,`. Go check them out
+        here:
+        `),f=i(u,"A",{href:!0,target:!0});var De=l(f);pe=n(De,"https://www.computerswithcauses.org/"),De.forEach(r),me=n(u,`. Your donation helps the charitable programs we support, provides much
+        needed educational and vocational assets, and substantially increases
+        resources to support our communities. Donate a Computer and write it off
+        on your taxes! Business donors can deduct the underappreciated value of
+        the computer, and individuals can deduct the current market value of a
+        computer. Tax Deductions! We may be able to give you a tax receipt. We
+        take your tech and donate it to `),T=i(u,"U",{});var Ge=l(T);ve=n(Ge,"Computers with Causes"),Ge.forEach(r),we=n(u,`. They, in
+        turn, will give the computer to people who are in need, such as
+        low-income students who need electronic devices for online school and
+        other purposes. If your tech is broken, as much as possible will be
+        salvaged, and then the rest will be recycled into new tech.
+        iCloud\u2122 locked phones are 100% accepted! iCloud\u2122 locked
+        phones are phones which you don't have a password for and don't have any
+        data you want on it, or phones that ask for a iCloud\u2122 password.
+        Any password locked phones that don't let you use a new user profile. We
+        will send a activation request to Apple`),q=i(u,"SMALL",{});var Oe=l(q);fe=n(Oe,"\xAE"),Oe.forEach(r),ye=n(u,`, with the
+        information of the phone we got. We may send you a email to remove it
+        from your iCloud\u2122 account`),u.forEach(r),o.forEach(r),this.h()},h(){p(m,"href","mailto:ISBtechdrive@hotmail.com"),p(m,"target","_blank"),p(v,"href","mailto:ISBtechdrive@hotmail.com"),p(v,"target","_blank"),p(w,"href","mailto:ISBtechdrive@hotmail.com"),p(w,"target","_blank"),p(f,"href","https://www.computerswithcauses.org/"),p(f,"target","_blank"),p(t,"id","faq")},m($,o){Ue($,t,o),e(t,y),e(y,H),e(t,Y),e(t,E),e(E,F),e(t,D),e(t,W),e(W,G),e(t,O),e(t,C),e(C,j),e(t,z),e(t,g),e(g,L),e(g,A),e(A,I),e(I,U),e(t,J),e(t,S),e(S,M),e(t,N),e(t,x),e(x,K),e(t,R),e(t,B),e(B,V),e(t,X),e(t,b),e(b,Z),e(b,m),e(m,ee),e(t,te),e(t,_),e(_,oe),e(_,v),e(v,ae),e(t,ne),e(t,k),e(k,re),e(k,w),e(w,se),e(t,ie),e(t,Q),e(Q,le),e(t,he),e(t,h),e(h,de),e(h,P),e(P,ce),e(h,ue),e(h,f),e(f,pe),e(h,me),e(h,T),e(T,ve),e(h,we),e(h,q),e(q,fe),e(h,ye)},p:Ee,i:Ee,o:Ee,d($){$&&r(t)}}}function Me(We,t,y){return[!0]}class Ke extends je{constructor(t){super(),ze(this,t,Me,Je,Le,{prerender:0})}get prerender(){return this.$$.ctx[0]}}export{Ke as default};
---
+++ _app/immutable/pages/techdrive/index.svelte-e0013d7f.js
@@ -0,0 +1,17 @@
+import{S as W,i as O,s as j,l as s,u as f,a as T,m as i,p as c,v,h as n,c as B,q,b as z,J as t,n as k}from"../../chunks/index-5a47a77b.js";function C(x){let e,o,u,m,d,y,b,r,g,h,l,w;return{c(){e=s("section"),o=s("h2"),u=f("What is the ISB tech drive?"),m=T(),d=s("p"),y=f(`The ISB tech drive is a chance for people in need to get computers,
+        phones, tablets, and other devices. If you have any old technology
+        (phones, computers, etc.), please donate it, even if it doesn't work or
+        if it is old, because we can fix it. Our goal is to stop e-waste and
+        provide everyone with what they need to learn.`),b=T(),r=s("p"),g=f(`What many people don't realize is there are many roadblocks holding
+        computers back for around a decade, and you can still use a 15-year-old
+        machine nicely now. That makes old computers great for us, because they
+        can still help kids in need with their learning. Your old device just
+        needs a chance. `),h=s("i"),l=s("b"),w=f("Give that chance to people in need!"),this.h()},l(p){e=i(p,"SECTION",{id:!0});var a=c(e);o=i(a,"H2",{});var I=c(o);u=v(I,"What is the ISB tech drive?"),I.forEach(n),m=B(a),d=i(a,"P",{});var $=c(d);y=v($,`The ISB tech drive is a chance for people in need to get computers,
+        phones, tablets, and other devices. If you have any old technology
+        (phones, computers, etc.), please donate it, even if it doesn't work or
+        if it is old, because we can fix it. Our goal is to stop e-waste and
+        provide everyone with what they need to learn.`),$.forEach(n),b=B(a),r=i(a,"P",{});var _=c(r);g=v(_,`What many people don't realize is there are many roadblocks holding
+        computers back for around a decade, and you can still use a 15-year-old
+        machine nicely now. That makes old computers great for us, because they
+        can still help kids in need with their learning. Your old device just
+        needs a chance. `),h=i(_,"I",{});var E=c(h);l=i(E,"B",{});var S=c(l);w=v(S,"Give that chance to people in need!"),S.forEach(n),E.forEach(n),_.forEach(n),a.forEach(n),this.h()},h(){q(e,"id","about")},m(p,a){z(p,e,a),t(e,o),t(o,u),t(e,m),t(e,d),t(d,y),t(e,b),t(e,r),t(r,g),t(r,h),t(h,l),t(l,w)},p:k,i:k,o:k,d(p){p&&n(e)}}}function G(x,e,o){return[!0]}class Y extends W{constructor(e){super(),O(this,e,G,C,j,{prerender:0})}get prerender(){return this.$$.ctx[0]}}export{Y as default};
